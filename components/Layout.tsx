0cab4d9092da738615fd74e6c79c262c28555730
Rayo Verweij <rayo.verweij@live.nl>
2022-02-09 16:25:40 +0000

    Send signup email

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { FormEvent, useState } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import s from '../styles/layout.module.scss'
 
 
@@ -9,6 +10,42 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
     const router = useRouter();
+
+
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [thanks, setThanks] = useState(false)
+    const [error, setError] = useState(false)
+
+    const signUp = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+        if(!firstName || !lastName || !email) return
+
+        const res = await fetch('/api/register', {
+            body: JSON.stringify({
+                firstName: firstName,
+                lastName: lastName,
+                email: email
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST'
+        })
+
+        if(res.status === 200) {
+            setThanks(true)
+            setFirstName("")
+            setLastName("")
+            setEmail("")
+        }
+        else {
+            setError(true)
+        }
+    }
+
 
     return (
         <div className={s.container}>
@@ -41,13 +78,42 @@ const Layout = ({ children }: LayoutProps) => {
                     <span className={s.signUp}>Sign up now!</span>
                     <span className={s.getAccess}><span className={s.manicule}>&#9758;</span> to get access to the <span className={s.preview}>free preview</span></span>
                     
-                    <form>
+                    <form onSubmit={signUp}>
                         <div className="formGroup">
-                            <input type="text" name="firstName" placeholder="First name" required />
-                            <input type="text" name="lastName" placeholder="Last name" required />
+                            <input
+                                type="text"
+                                name="firstName"
+                                value={firstName}
+                                onChange={e => setFirstName(e.currentTarget.value)}
+                                placeholder="First name"
+                                required
+                            />
+                            <input
+                                type="text"
+                                name="lastName"
+                                value={lastName}
+                                onChange={e => setLastName(e.currentTarget.value)}
+                                placeholder="Last name"
+                                required
+                            />
                         </div>
-                        <input type="email" name="email" placeholder="Email" required />
-                        <button type="submit">Sign up</button>
+                        <input
+                            type="email"
+                            name="email"
+                            value={email}
+                            onChange={e => setEmail(e.currentTarget.value)}
+                            placeholder="Email"
+                            required
+                        />
+                        { thanks &&
+                            <p>Thanks for signing up! You will receive an invitation shortly.</p>
+                        }
+                        { error &&
+                            <p>Whoops - something went wrong, try again later!</p>
+                        }
+                        { !thanks &&
+                            <button type="submit">Sign up</button>
+                        }
                     </form>
                 </aside>
             </header>
---
+++ pages/api/register.tsx
@@ -0,0 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+
+type ResData = {
+    user: string
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<ResData>) {
+    const nodemailer = require("nodemailer")
+
+    const transporter = nodemailer.createTransport({
+        port: 465,
+        host: "smtp.gmail.com",
+        auth: {
+            user: "ubok.website.notifier@gmail.com",
+            pass: "v6$@Lilkah"
+        },
+        secure: true
+    })
+
+    const mailData = {
+        from: 'ubok.website.notifier@gmail.com',
+        to: 'cameron.macalister@voxsio.com',
+        subject: `Website demo request: ${req.body.firstName} ${req.body.lastName}`,
+        text: `Hi there,\n\n${req.body.firstName} ${req.body.lastName} at ${req.body.email} has requested access to the app via the form on the website.\n\nCheers,\nThe Website\n`
+    }
+
+    transporter.sendMail(mailData, (err: any, info: any) => {
+        if(err) console.log(err)
+        else console.log(info)
+    })
+
+    res.status(200).end()
+}
