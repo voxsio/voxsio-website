3415d1fd88137995218f54f1296625fa797c8c65
rayoverweij <rayo.verweij@live.nl>
2023-01-16 12:07:24 +0000

    Serverless fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	target: "serverless"
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
