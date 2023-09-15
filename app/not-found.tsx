87a4d3dc71da5a81a4ac08e0885fa8d207f41525
rayoverweij <rayo.verweij@live.nl>
2023-09-15 18:54:11 +0100

    404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,24 +2,24 @@ import Image from 'next/image';
 
 
 const NotFound = () => (
-	<div className="w-full h-[500px] flex flex-row bg-sky">
+	<section className="w-full min-h-[75vh] lg:min-h-[65vh] mt-12 lg:flex items-center justify-evenly">
 		<Image
-			src="/images/ubee-internet.png"
-			alt=""
-			width={500}
-			height={500}
-			className="ml-[20%] mr-[10%]"
+			src={`/images/ubee-internet.png`}
+			alt="Our mascot Ubee pointing to an icon representing disconnected internet"
+			width={300}
+			height={300}
+			className="mx-auto lg:mx-0 border-b-2 border-dark-500"
 			priority
 		/>
-		<div className="flex flex-col justify-center">
-			<h1 className="font-hand font-black text-9xl">
+		<div className="lg:w-96 mt-12 lg:mt-0">
+			<h1 className="font-hand font-bold text-8xl text-center lg:text-left">
 				404
 			</h1>
-			<p className="font-hand text-5xl">
+			<p className="mt-4 text-4xl text-center lg:text-left">
 				Page not found!
 			</p>
 		</div>
-	</div>
+	</section>
 );
 
 export default NotFound;
