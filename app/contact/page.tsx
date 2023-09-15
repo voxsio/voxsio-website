89f12156d7bdce418dc328528119221fa4783a4b
rayoverweij <rayo.verweij@live.nl>
2023-09-15 17:59:06 +0100

    Contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,22 +1,18 @@
-import Image from 'next/image';
-
-
 const Page = () => (
-	<div className="px-8 py-20 bg-light-500 rounded-[4rem]">
-		<div className="max-w-[500px] mx-auto">
-			<p>Get in touch! We'd love to hear from you at <a href="mailto:chat@ubok.app">chat@ubok.app</a>.</p>
+	<div className="px-8 py-20 mb-16 min-h-[70vh]">
+		<div className="max-w-[500px] mx-auto p-8 bg-light-500 border-2 border-light-700 rounded-lg">
+			<p>
+				Get in touch! We'd love to hear from you at <a href="mailto:chat@ubok.app">chat@ubok.app</a>.
+			</p>
 
-			<p>Alternatively, come say hi in person! We are based in the Bayes Centre at the University of Edinburgh and we are always open for visitors.</p>
-			<div className="relative w-[200px] aspect-[125/27] mt-8 ml-[calc(50%-250px)]">
-				<Image
-					src="/images/bayes.png"
-					alt="The Bayes Centre wordmark"
-					fill
-				/>
-			</div>
-			<address className="not-italic">
-				<b>Bayes Centre</b><br />
+			<p className="mt-8">
+				Alternatively, come say hi in person! We are based in the Bayes Centre at the University of Edinburgh and we are always open for visitors.
+			</p>
+			
+			<address className="not-italic mt-8">
+				<strong>Voxsio Ltd</strong><br />
 				Office 4.35<br />
+				Bayes Centre<br />
 				47 Potterrow<br />
 				Edinburgh EH8 9BT
 			</address>
